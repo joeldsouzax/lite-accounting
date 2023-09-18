f2dcbd4c3d8702aa1e67f5ea35f94fcd38310604
Joel DSouza <joeldsouzax@gmail.com>
2023-09-18 20:27:25 +0200

    build(database): added generate types script from supabase

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+/*
+ * Filename: /Users/joel/Code/start-up/lite-accounting/middleware.ts
+ * Path: /Users/joel/Code/start-up/lite-accounting
+ * Created Date: Monday, September 18th 2023, 8:22:38 pm
+ * Author: Joel Dsouza
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
