83be663ba4050a2ee736a3034fa0513656b2003e
Joel DSouza <joeldsouzax@gmail.com>
2023-09-18 15:15:31 +0200

    refactor(landing page): cleaned up the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-  <main>LitRengskap</main>
+  <main className='flex flex-col justify-center items-center w-full'>LitRengskap</main>
   )
 }
