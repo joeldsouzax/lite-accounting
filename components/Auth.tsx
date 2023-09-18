ddaf387ba6c9e8d9d6c0bbf09dd87443af668a6e
Joel DSouza <joeldsouzax@gmail.com>
2023-09-18 23:14:06 +0200

    refactor(favicon): added favicon to the app

---
+++ components/Auth.tsx
@@ -0,0 +1,16 @@
+/*
+ * Filename: /Users/joel/Code/start-up/lite-accounting/components/Auth.tsx
+ * Path: /Users/joel/Code/start-up/lite-accounting
+ * Created Date: Monday, September 18th 2023, 10:47:01 pm
+ * Author: Joel Dsouza
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+import * as React from 'react';
+
+const Auth: React.FC = () => {
+  return <div />;
+};
+
+export default Auth;
