713552723a6c8fa6a9e1023511a75157e80eb1ea
Joel DSouza <joeldsouzax@gmail.com>
2023-09-18 22:46:05 +0200

    refactor(sign-in page): added header to sign in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,19 @@
+import Logo from '@/components/Logo';
+import ThemeSelector from '@/components/ThemeSelector';
+
 export default async function SignIn() {
   return (
     <main className="flex flex-col justify-center items-center w-full">
-      Sign in
+      <header className="max-w-xs w-full navbar lg:max-w-lg transition-all ease-out duration-100">
+        <nav className="navbar-start">
+          <a href="/">
+            <Logo />
+          </a>
+        </nav>
+        <nav className="navbar-end flex gap-4">
+          <ThemeSelector />
+        </nav>
+      </header>
     </main>
   );
 }
