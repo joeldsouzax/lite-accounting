1a139c95345c9474562c9155f9da29fe85ee5e72
Joel DSouza <joeldsouzax@gmail.com>
2023-09-18 15:16:27 +0200

    build(lite-accounting): added prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-  <main className='flex flex-col justify-center items-center w-full'>LitRengskap</main>
-  )
+    <main className="flex flex-col justify-center items-center w-full">
+      LitRengskap
+    </main>
+  );
 }
