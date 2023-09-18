78bac95e3d4107cd36192e63d9069504411e5e55
Joel DSouza <joeldsouzax@gmail.com>
2023-09-18 16:39:39 +0200

    refactor(landing page): added gap for stacked column flex layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ThemeSelector from '@/components/ThemeSelector';
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center w-full">
+    <main className="flex flex-col justify-center gap-2 items-center w-full">
       <header className="max-w-xs w-full navbar lg:max-w-lg transition-all ease-out duration-75">
         <nav className="navbar-start">liteRengskap</nav>
         <nav className="navbar-end">
