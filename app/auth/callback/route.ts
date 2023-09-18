323b72e7ee10abb9d1e990d226c865cc7f7f73bd
Joel DSouza <joeldsouzax@gmail.com>
2023-09-18 21:08:21 +0200

    refactor(auth): added supabase auth callback route
    required for server side auth flow implementation

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,26 @@
+/*
+ * Filename: /Users/joel/Code/start-up/lite-accounting/app/auth/callback/route.ts
+ * Path: /Users/joel/Code/start-up/lite-accounting
+ * Created Date: Monday, September 18th 2023, 9:03:14 pm
+ * Author: Joel Dsouza
+ *
+ * Copyright (c) 2023 Your Company
+ */
+
+import { Database } from '@/database.types';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse, type NextRequest } from 'next/server';
+import { cookies } from 'next/headers';
+
+export async function GET(req: NextRequest) {
+  const requestUrl = new URL(req.url);
+  const code = requestUrl.searchParams.get('code');
+
+  if (code) {
+    const supabase = createRouteHandlerClient<Database>({ cookies });
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  // URL to redirect to after signin process is complete
+  return NextResponse.redirect(requestUrl.origin);
+}
