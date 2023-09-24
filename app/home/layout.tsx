4a270f1415664091b9c342fe69c9b21507639aae
Joel DSouza <joeldsouzax@gmail.com>
2023-09-24 17:12:15 +0200

    refactor(protected routes): added all the routes under home in protected route

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,10 +1,23 @@
-export default async function HomeLayout() {
+import { getSession, getUserDetails } from '../supabase-server';
+import { redirect } from 'next/navigation';
+
+export const dynamic = 'force-dynamic';
+export default async function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getSession();
+
+  if (!session) {
+    return redirect('/sign-in');
+  }
   return (
     <main
       id="lite-regnskap-home"
       className="px-4 md:px-2 container md:max-w-md"
     >
-      Hello
+      {children}
     </main>
   );
 }
