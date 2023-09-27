cc2e58f5410032c5266ac4956f7361310da4a9b6
Joel DSouza <joeldsouzax@gmail.com>
2023-09-27 17:36:15 +0200

    refactor(home): added max width for home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,12 @@
 export default async function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <section
+      id="user-settings"
+      className="px-4 md:px-2 container md:max-w-md mb-4 md:mb-6 transition-all ease-linear duration-100"
+    >
+      <article className="prose">
+        <h2>Home</h2>
+      </article>
+    </section>
+  );
 }
