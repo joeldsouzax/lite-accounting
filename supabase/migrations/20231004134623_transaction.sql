b1b61746aa222c9228af1946c6287fc1af8a0710
Joel DSouza <joeldsouzax@gmail.com>
2023-10-05 16:53:01 +0200

    refactor(transactions schema): added accounts and entries schema with amount

--- supabase/migrations/20231004134623_transaction.sql
+++ supabase/migrations/20231004134623_transaction.sql
@@ -1,16 +1,44 @@
---- account table
+-- account table
+-- every user will have their own account table, a short cut for them to save their most usable accounts maybe.
+-- deleting the user will also delete all the accounts this user has saved till now
 create table accounts(
-    id integer primary key generated as identity,
-    created_at timestamp with time zone not null default now(),
-    name text not null,
-    description text,
-);
-
--- entries table which holds the basic atom transaction entry
-create table entries(
     id integer primary key generated always as identity,
     created_at timestamp with time zone not null default now(),
-    name text,
     updated_at timestamp with time zone,
-    transaction_date_time timestamp with time zone not null,  
+    name varchar(160) not null,
+    account_code integer not null,
+    description text,
+    user_id uuid,
+    constraint accounts_user_id_fkey foreign key (user_id) references auth.users(id) on delete cascade
+);
+
+-- enable row level security for accounts
+alter table accounts enable row level security;
+create policy "users can read their own accounts" on accounts for select to authenticated using (auth.uid() = user_id);
+create policy "users can delete their own accounts" on accounts for delete to authenticated using (auth.uid() = users_id);
+create policy "users can update their own accounts" on accounts for update to authenticated using (auth.uid() = users_id);
+create policy "only authenticated users can insert" on accounts for inster to authenticated using (true);
+
+
+-- entries table which holds the basic atom transaction entry
+-- amount uses bigint
+-- since we are only going to use one currency (nok) we do not need to store the magnifier for this currency
+-- for reference https://www.linkedin.com/pulse/one-way-store-money-postgresql-database-benchmark-against-jason-lui/
+create table entries(
+    id bigint primary key generated always as identity,
+    created_at timestamp with time zone not null default now(),
+    updated_at timestamp with time zone,
+    transaction_date timestamp with time zone not null default now(),
+    amount bigint not null check (amount > 0),
+    name varchar(160) not null,
+    description text,
+    credit integer not null,
+    debit integer not null,
+    constraint entries_credit_fkey foreign key (credit) references public.accounts(id) on delete restrict,
+    constraint entries_debit_fkey foreign key (debit) references public.accounts(id) on delete restrict
+    -- deletes are restricted because deleting an account with outstanding
+    -- entries does not make sense. if the account's balance is non zero,
+    -- it would make assets or liabilities vanish, and even if its zero,
+    -- the account is responsible for the non zero balances of other accounts,
+    -- so deleting it would lose important information.
 );
